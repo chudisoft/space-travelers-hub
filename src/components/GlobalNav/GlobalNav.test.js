a689857205ba3b870e10d5c087de6f6472644495
christopheruduekwe@gmail.com <christopheruduekwe@gmail.com>
2023-08-28 13:04:02 +0100

    Project cleanup

--- src/components/GlobalNav/GlobalNav.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import GlobalNav from './GlobalNav';
-
-test('renders learn react link', () => {
-  render(<GlobalNav />);
-  const linkElement = screen.getByText(/Math Magicians/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- src/index.js
+++ src/index.js
@@ -5,14 +5,11 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from 'react-router-dom';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ErrorPage from './routes/error-page';
 import Home from './components/Home/Home';
-import Categories from './components/Categories/Categories';
-import Books from './components/Books/Books';
-import store from './redux/store';
 
 const router = createBrowserRouter([
   {
@@ -24,27 +21,15 @@ const router = createBrowserRouter([
         path: '/',
         element: <Home />,
       },
-      {
-        path: 'Home/',
-        element: <Home />,
-      },
-      {
-        path: 'Books/',
-        element: <Books />,
-      },
-      {
-        path: 'Categories/',
-        element: <Categories />,
-      },
     ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    {/* <Provider store={store}> */}
+    <RouterProvider router={router} />
+    {/* </Provider> */}
   </React.StrictMode>,
 );
 
