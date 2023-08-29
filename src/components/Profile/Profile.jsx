f5405e5836f3a1c4e6e1900269fcb91fbbad1768
christopheruduekwe@gmail.com <christopheruduekwe@gmail.com>
2023-08-29 15:58:37 +0100

    Display My rockets and My missions

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,26 +7,34 @@ import './Profile.css';
 function Profile() {  
   const dispatch = useDispatch();
   const error = useSelector((state) => state.missions.error);
-  const missionsAvailable = useSelector((state) => state.missions.missions);
+  const missions = useSelector((state) => state.missions.missions);
   // get rockets data from the store
   const rockets = useSelector(state => state.rockets);
 
   return (
     <div className='w-100 p-4'>
-      <div className='missions'>
-        {
-          missionsAvailable.filter((x) => x.reserved === true).map((b) => 
-            <div className='mission'>{b.mission_name}</div>
-          )
-        }
+      <div className='profile-row'>
+        <div className='profile-col'>
+          <h3>My Missions</h3>
+          <div className='profiles'>
+            {
+              missions.filter((x) => x.reserved === true).map((b) => 
+                <div className='profile'>{b.mission_name}</div>
+              )
+            }
+          </div>
+        </div>
+        <div className='profile-col'>
+          <h3>My Rockets</h3>
+          <div className='profiles'>
+            {
+              rockets.filter((x) => x.reserved === true).map((b) => 
+                <div className='profile'>{b.rocket_name}</div>
+              )
+            }
+          </div>
+        </div>
       </div>
-      {/* <div className='missions'>
-        {
-          missionsAvailable.map((b) => 
-            <div className='mission'>{b.mission_name}</div>
-          )
-        }
-      </div> */}
     </div>
   )
 }
